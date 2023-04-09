cb91bb5eab22b4cf5158abadc737b921f691fb21
Daniel Romero <tutorialesdanielromero@gmail.com>
2023-04-09 10:48:01 -0500

    obtener usuarios

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,20 @@
+import Users from '@/components/Users';
 import React from 'react';
 
-function IndexPage() {
-  return <div>IndexPage</div>;
+const fetchUsers = async () => {
+  const resp = await fetch('https://reqres.in/api/users');
+  const data = await resp.json();
+  return data.data;
+};
+
+async function IndexPage() {
+  const users = await fetchUsers();
+  return (
+    <div>
+      <h1>IndexPage</h1>
+      <Users users={users} />
+    </div>
+  );
 }
 
 export default IndexPage;
---
+++ components/Users.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Users = ({ users }) => {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <div>
+            <h5>
+              {user.id} {user.first_name} {user.last_name}
+            </h5>
+            <p>{user.email}</p>
+          </div>
+          <img
+            src={user.avatar}
+            alt={user.avatar}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Users;
